390e700a30ca2d6d5dfbb062ea7637c243d5e8a8
Lucas <lucasschimidtt16@gmail.com>
2026-05-14 23:02:54 -0300

    feat: create Vehicles models

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from clients.models import Client
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='vehicles')
+    plate = models.CharField(max_length=10, unique=True)
+    brand = models.CharField(max_length=50)
+    vehicle_model = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    year = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.brand} {self.model} {self.plate}'
