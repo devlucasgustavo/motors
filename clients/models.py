009c3979597f9d5b22254025eb9ba4603698061c
Lucas <lucasschimidtt16@gmail.com>
2026-05-14 22:49:50 -0300

    feat: create models Clients

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=14, unique=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
+    address = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Meta:
+    ordering = ['name']
